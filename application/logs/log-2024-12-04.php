0acfa21615d909ec96d69f45d228ba6d4c64f2bf
syahputra601 <ajisyahputra601@gmail.com>
2024-12-04 16:11:17 +0700

    edit file controller spk_delivery dan Model spk_delivery_model

--- application/logs/log-2024-12-04.php
+++ application/logs/log-2024-12-04.php
@@ -2,3 +2,13 @@
 
 ERROR - 2024-12-04 03:29:09 --> 404 Page Not Found: /index
 ERROR - 2024-12-04 11:23:00 --> Severity: Notice --> Undefined index: project C:\xampp_5_6\htdocs\hirobolt\application\modules\spk_delivery_sj\views\confirm.php 35
+ERROR - 2024-12-04 08:35:57 --> 404 Page Not Found: /index
+ERROR - 2024-12-04 15:56:19 --> Severity: error --> Exception: ERROR n°6 : Impossible to load the image C:/xampp_5_6/htdocshirobolt/assets/foto/LogoHiroBolt1.png C:\xampp_5_6\htdocs\hirobolt\assets\html2pdf\html2pdf\html2pdf.class.php 1319
+ERROR - 2024-12-04 15:58:13 --> Severity: error --> Exception: ERROR n°6 : Impossible to load the image C:/xampp_5_6/htdocshirobolt/assets/foto/LogoHiroBolt1.png C:\xampp_5_6\htdocs\hirobolt\assets\html2pdf\html2pdf\html2pdf.class.php 1319
+ERROR - 2024-12-04 15:58:22 --> Severity: error --> Exception: ERROR n°6 : Impossible to load the image C:/xampp_5_6/htdocshirobolt/assets/foto/LogoHiroBolt1.png C:\xampp_5_6\htdocs\hirobolt\assets\html2pdf\html2pdf\html2pdf.class.php 1319
+ERROR - 2024-12-04 16:05:47 --> Severity: error --> Exception: ERROR n°6 : Impossible to load the image C:/xampp_5_6/htdocshirobolt/assets/foto/LogoHiroBolt1.png C:\xampp_5_6\htdocs\hirobolt\assets\html2pdf\html2pdf\html2pdf.class.php 1319
+ERROR - 2024-12-04 16:05:54 --> Severity: error --> Exception: ERROR n°6 : Impossible to load the image C:/xampp_5_6/htdocshirobolt/assets/foto/LogoHiroBolt1.png C:\xampp_5_6\htdocs\hirobolt\assets\html2pdf\html2pdf\html2pdf.class.php 1319
+ERROR - 2024-12-04 16:06:57 --> Severity: error --> Exception: ERROR n°6 : Impossible to load the image C:/xampp_5_6/htdocshirobolt/assets/foto/LogoHiroBolt1.png C:\xampp_5_6\htdocs\hirobolt\assets\html2pdf\html2pdf\html2pdf.class.php 1319
+ERROR - 2024-12-04 16:07:08 --> Severity: error --> Exception: ERROR n°6 : Impossible to load the image C:/xampp_5_6/htdocshirobolt/assets/foto/LogoHiroBolt1.png C:\xampp_5_6\htdocs\hirobolt\assets\html2pdf\html2pdf\html2pdf.class.php 1319
+ERROR - 2024-12-04 16:07:50 --> Severity: error --> Exception: ERROR n°6 : Impossible to load the image C:/xampp_5_6/htdocshirobolt/assets/foto/LogoHiroBolt1.png C:\xampp_5_6\htdocs\hirobolt\assets\html2pdf\html2pdf\html2pdf.class.php 1319
+ERROR - 2024-12-04 16:07:56 --> Severity: error --> Exception: ERROR n°6 : Impossible to load the image C:/xampp_5_6/htdocshirobolt/assets/foto/LogoHiroBolt1.png C:\xampp_5_6\htdocs\hirobolt\assets\html2pdf\html2pdf\html2pdf.class.php 1319
