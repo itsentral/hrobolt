b6d835f9e1faa45149088578937d8d2711409d12
syahputra601 <ajisyahputra601@gmail.com>
2024-12-13 10:52:11 +0700

    editing helper getstockbarangAll by aji

---
+++ application/logs/log-2024-12-13.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-12-13 04:33:05 --> 404 Page Not Found: /index
+ERROR - 2024-12-13 10:37:33 --> Query error: Unknown column 'a.id_gudang' in 'field list' - Invalid query: SELECT `a`.`id_gudang`, `a`.`kode_gudang`
+FROM `warehouse` `a`
+ERROR - 2024-12-13 10:37:51 --> Query error: Unknown column 'a.id_gudang' in 'field list' - Invalid query: SELECT `a`.`id_gudang`, `a`.`kode_gudang`
+FROM `warehouse` `a`
