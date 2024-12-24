7d261f278da538b15da6ed69a78038052f7761ca
syahputra601 <ajisyahputra601@gmail.com>
2024-12-24 14:34:25 +0700

    edit 2

--- application/logs/log-2024-12-24.php
+++ application/logs/log-2024-12-24.php
@@ -3,3 +3,6 @@
 ERROR - 2024-12-24 03:44:08 --> 404 Page Not Found: /index
 ERROR - 2024-12-24 08:25:40 --> 404 Page Not Found: /index
 ERROR - 2024-12-24 08:29:37 --> 404 Page Not Found: /index
+ERROR - 2024-12-24 08:33:56 --> 404 Page Not Found: /index
+ERROR - 2024-12-24 08:33:58 --> 404 Page Not Found: /index
+ERROR - 2024-12-24 08:33:58 --> 404 Page Not Found: /index
